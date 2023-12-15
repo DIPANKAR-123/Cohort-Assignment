eef778cd302c5d8d92213149e9fc359cd788d74c
Dipankar <dipankaryadav1234@gmail.com>
2023-12-15 21:30:29 +0530

    week1 code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
-
+  // var temp1 = str1.split("").sort().join("");
+  var temp2 = str2.toLowerCase().split("").sort().join("");
+  var temp1 = str1.toLowerCase().split("").sort().join("");
+  // var temp2 = str2.sort();
+  // document.write()
+  // console.log(temp1, temp2);
+  console.log("hellp",str1,str2,temp1,temp2);
+  return temp1 == temp2;
 }
 
+isAnagram("hello","hemlo");
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -6,7 +6,47 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = {};
+  const transac = new Map();
+  for(var i =0;i<transactions.length;i++) 
+  { 
+    const {category,price}=transactions[i];
+    console.log(price,category);
+    if(transac.has(category)){
+      var currval=transac.get(category);
+      transac.set(category,currval+price);
+    }else{
+
+      transac.set(category,price);
+    }
+  }
+  console.log(transac);
+  const ans =[]
+  for (const x of transac.keys()) {
+     
+    var totalPrice=transac.get(x);
+    const obj1={category:x,totalSpent:totalPrice};
+    ans.push(obj1);
+    console.log(totalPrice);
+  }
+  return ans;
+ 
 }
+calculateTotalSpentByCategory([
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656076800000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+])
 
 module.exports = calculateTotalSpentByCategory;
